c66885371daba88ecb962aa996e2ec62deadf5c2
Olivier PEDOUSSAUT <opedoussaut@gmail.com>
2025-06-06 18:48:19 +0200

    🚀 Deploy 3DSwymReportGenerator widget and wrapper to GitHub Pages

--- gh-pages/3DSwymReportGeneratorWrapper/main.js
+++ gh-pages/3DSwymReportGeneratorWrapper/main.js
@@ -1,16 +1,1 @@
-define('3DSwymReportGeneratorWrapper', [], function () {
-  'use strict';
-
-  const widgetUrl = 'https://opedoussaut.github.io/AI4HT/gh-pages/3DSwymReportGenerator/';
-
-  return {
-    onLoad: function () {
-      const iframe = document.createElement('iframe');
-      iframe.src = widgetUrl;
-      iframe.style.width = '100%';
-      iframe.style.height = '100%';
-      iframe.style.border = 'none';
-      document.body.appendChild(iframe);
-    }
-  };
-});
+window.location.href = '../3DSwymReportGenerator/index.html';
