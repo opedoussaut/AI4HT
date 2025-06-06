e3e9956a52de5aab0ef3a498a89119693dae426f
Olivier PEDOUSSAUT <opedoussaut@gmail.com>
2025-06-06 10:52:43 +0200

    Re-add AIWIDGETS as regular folder

---
+++ AIWIDGETS/3DSwymReportGenerator/src/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>3DSwym Report Generator</h1>
+    <p>This widget summarizes SWYM conversations and communities using Hugging Face LLMs.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #3c3c3c;
+}
+</style>
---
+++ AIWIDGETS/3DSwymReportGenerator/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      vue: '@vue/runtime-dom'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
